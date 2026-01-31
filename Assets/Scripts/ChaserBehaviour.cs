81bf3b32280df1c096953d655a78630c4828a893
Eugene Chew <31818408+ehjc1@users.noreply.github.com>
2026-01-31 21:28:44 +1300

    Start Scene

--- Assets/Scripts/ChaserBehaviour.cs
+++ Assets/Scripts/ChaserBehaviour.cs
@@ -2,23 +2,30 @@
 using Unity.VectorGraphics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class ChaserBehaviour : MonoBehaviour
 {
 
     public float chaserSpeed = 5f;
+    public float chaserDelayTime = 5f;
     private bool isColliding = false;
+    private float elapsedTime = 0f;
+    GameObject player = GameObject.FindWithTag("Player");
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isColliding)
+        elapsedTime += Time.deltaTime;
+        Debug.Log("Elapsed Time: " + elapsedTime);
+        if (!isColliding && elapsedTime >= chaserDelayTime)
         {
+            Debug.Log("Chaser Moving");
             transform.position -= Vector3.left * chaserSpeed * Time.deltaTime;
         }
     }
@@ -28,7 +35,6 @@ public class ChaserBehaviour : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             isColliding = true;
-            //Camera.main.GetComponent<ChaserBehaviour>().enabled = false;
             ReloadGame();
         }
     }
